e97e1fe4ace2009242d115eff813145c1334942a
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-02-17 03:17:32 +0100

    Authentication routes and groups for users and admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminCheckMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//User routes
+Route::middleware('auth')->group(function () {
+
+});
+
+//Admin routes
+Route::middleware(['auth', AdminCheckMiddleware::class])->prefix('/admin')->group(function () {
+
+
+});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
